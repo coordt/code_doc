0e3149f27e38a4ee788d85b1c3be7f7e98a26067
Raffi Enficiaud <raffi.enficiaud@tuebingen.mpg.de>
2014-07-02 10:27:42 +0200

    SWD-67
    Maintainer page
    Fixing bug with bootstrap/jQuery

--- code_doc/urls.py
+++ code_doc/urls.py
@@ -11,9 +11,12 @@ from code_doc import views
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
   url(r'^accounts/login/$',  login, name='login'),
-  url(r'^accounts/logout/$', logout),  
-  url(r'^project/(?P<project_id>\d+)/$', views.detail_project, name='project'),
-  url(r'^project2/(?P<project_id>\d+)/$', views.ProjectView.as_view(), name='project2'),
+  url(r'^accounts/logout/$', logout, name='logout'),  
+  url(r'^project/(?P<project_id>\d+)/$', views.ProjectView.as_view(), name='project'),
+  
+  url(r'^maintainer/$', views.MaintainerProfileView.as_view(), name='maintainer'),
+  
+  
   url(r'^author/(?P<author_id>\d+)/$', views.detail_author, name='author'),
   url(r'^api/artifact/(?P<project_version_id>\d+)/(?P<filename>.+)$', views.FileUploadView.as_view(), name='fileupload'),
   url(r'^api/artifact/(?P<project_version_id>\d+)/$', views.FileUploadView.as_view(), name='fileupload_post'),
--- code_doc/views.py
+++ code_doc/views.py
@@ -6,46 +6,75 @@ from django.http import Http404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import View
+
+
+
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser, FileUploadParser
 from rest_framework.views import APIView
 
+
+
 from code_doc.models import Project, Author, Topic
 #from code_doc.serializers import ProjectSerializer
 
-from django.contrib.auth.decorators import login_required
 
 
 
 def index(request):
   projects_list = Project.objects.order_by('name')
   topics_list = Topic.objects.order_by('name')
-  return render(request, 'code_doc/index.html', {'projects_list': projects_list, 'topics_list': topics_list})
-  
-  
-def detail_project(request, project_id):
-  try:
-    project = Project.objects.get(pk=project_id)
-  except Project.DoesNotExist:
-    raise Http404
-  
-  author_list = project.authors.all()
-  topic_list  = project.topics.all()
   return render(
-            request, 
-            'code_doc/project_details.html', 
-            {'project': project, 
-             'authors': author_list, 
-             'topics': topic_list})
+          request, 
+          'code_doc/index.html', 
+          {'projects_list': projects_list, 
+           'topics_list': topics_list})
+  
+
+
+class MaintainerProfileView(View):
+  
+  @method_decorator(login_required(login_url='/accounts/login/'))
+  def get(self, request):
+    try:
+      user = request.user #User.objects.get(pk=user_id)
+    except Project.DoesNotExist:
+      raise Http404
+    
+    projects = Project.objects.filter(administrators=user)
+    return render(
+              request, 
+              'code_doc/maintainer_details.html', 
+              {'projects': projects})
+  
+  @login_required(login_url='/accounts/login/')
+  def post(self, request):
+    pass
 
 
 
-class ProjectView(APIView):
+
+
+class ProjectView(View):
   
   def get(self, request, project_id):
-    return detail_project(request, project_id)
-    pass
+    try:
+      project = Project.objects.get(pk=project_id)
+    except Project.DoesNotExist:
+      raise Http404
+    
+    author_list = project.authors.all()
+    topic_list  = project.topics.all()
+    return render(
+              request, 
+              'code_doc/project_details.html', 
+              {'project': project, 
+               'authors': author_list, 
+               'topics': topic_list})
   
   @login_required(login_url='/accounts/login/')
   def post(self):
