f255d0b36ce7546eecddfc68dcacd6f3fc6a44c5
Raffi Enficiaud <raffi.enficiaud@tuebingen.mpg.de>
2014-10-31 16:05:45 +0100

    SWD-234
    Log now with correct aspect ratio

--- code_doc/templatetags/project_image.py
+++ code_doc/templatetags/project_image.py
@@ -7,5 +7,15 @@ register = template.Library()
 @register.inclusion_tag('code_doc/tags/project_image_tag.html')
 def project_image(project_id, size = None):
   project = Project.objects.get(pk=project_id)
+  
+  im = project.icon
+  if(im.width > im.height):
+    size_x = size
+    size_y = im.height * size_x / im.width
+  else:
+    size_y = size
+    size_x = im.width * size_y / im.height
+  
   return {'image': project.icon,
-          'size' : size}
+          'size_x' : size_x,
+          'size_y' : size_y}
