4e2112b843fbc226c49a265bb7903888032695dc
Raffi Enficiaud <raffi.enficiaud@tuebingen.mpg.de>
2014-11-20 16:08:05 +0100

    Deployment on a real server

--- SoWDocumentation/urls.py
+++ SoWDocumentation/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^code_doc/', include('code_doc.urls')),
+    url(r'^', include('code_doc.urls')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
