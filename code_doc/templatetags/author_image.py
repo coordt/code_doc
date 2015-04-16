8bc36017838df6bdc34e03c19657d0019c40b447
Stephan Wenninger <swenninger@tuebingen.mpg.de>
2015-04-16 15:35:03 +0200

    Centered text in SVG image. Varying color of account pictures. Better Project-Details-View

--- code_doc/templatetags/author_image.py
+++ code_doc/templatetags/author_image.py
@@ -1,13 +1,29 @@
 
 from django import template
 from code_doc.models import Author
+
+import hashlib
+
 register = template.Library()
- 
 
 @register.inclusion_tag('code_doc/tags/author_image_tag.html')
 def author_image(author_id, size = None):
   author = Author.objects.get(pk=author_id)
-  return {'image': None, #author.image,
+  return {'uploaded_image': None, #author.image,
           'gravatar_email': None if author.gravatar_email == "" else author.gravatar_email,
-          'author_initial': author.firstname[0], 
+          'author_initial': author.firstname[0],
+          'background_color': hash_string_to_color_hex(author.firstname + author.lastname, 200),
           'size' : size}
+
+def hash_string_to_color_hex(str, brightness_limit):
+  hashed = hashlib.md5(str.encode('utf-8')).hexdigest()[:6]
+  r = int(hashed[0:2], 16)
+  g = int(hashed[2:4], 16)
+  b = int(hashed[4:6], 16)
+
+  r = min(r, brightness_limit)
+  g = min(g, brightness_limit)
+  b = min(b, brightness_limit)
+
+  result = '#%02x%02x%02x' % (r, g, b)
+  return result
